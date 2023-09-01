cf09ecf22212f34c3b72c7128c28c7dee99e22b8
zhangshouzhong2021 <zhangshouzhong@kaihong.com>
2023-09-01 18:29:05 +0800

    增加opp传输功能在场景测试，profile测试中有opp的api接口
    Signed-off-by: zhangshouzhong@kaihong.com <zhangshouzhong@kaihong.com>

--- test/example/bluetoothtest/entry/src/main/ets/MainAbility/MainAbility.ts
+++ test/example/bluetoothtest/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,7 +28,8 @@ export default class MainAbility extends Ability {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
     hilog.info(0x0000, 'testTag', '%{public}s', 'want param:' + JSON.stringify(want) ?? '');
     hilog.info(0x0000, 'testTag', '%{public}s', 'launchParam:' + JSON.stringify(launchParam) ?? '');
-    this.context.requestPermissionsFromUser(PERMISSIONS)
+    this.context.requestPermissionsFromUser(PERMISSIONS);
+    globalThis.context = this.context;
   }
 
   onDestroy() {
