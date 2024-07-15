b1efadfcf423a40c6ad75c0b24361979f3e5cd5a
冯鹏义 <fengpengyi1@huawei.com>
2024-07-15 21:46:06 +0800

    add inner_api
    Signed-off-by: 冯鹏义 <fengpengyi1@huawei.com>

--- services/bluetooth/server/src/bluetooth_host_server.cpp
+++ services/bluetooth/server/src/bluetooth_host_server.cpp
@@ -1807,7 +1807,7 @@ int32_t BluetoothHostServer::RestrictBluetooth()
     return BT_ERR_API_NOT_SUPPORT;
 }
 
-int32_t BluetoothHostServer::SatelliteApdapter(int state)
+int32_t BluetoothHostServer::SatelliteAdapter(int state)
 {
     return BT_ERR_API_NOT_SUPPORT;
 }
