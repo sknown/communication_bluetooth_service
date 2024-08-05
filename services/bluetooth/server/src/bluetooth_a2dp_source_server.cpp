8b43c9832da5eaceb59fc81b77db0dc0ea10016d
冯鹏义 <fengpengyi1@huawei.com>
2024-08-05 09:10:50 +0800

    modify GetRenderPosition
    Signed-off-by: 冯鹏义 <fengpengyi1@huawei.com>

--- services/bluetooth/server/src/bluetooth_a2dp_source_server.cpp
+++ services/bluetooth/server/src/bluetooth_a2dp_source_server.cpp
@@ -420,9 +420,10 @@ int BluetoothA2dpSourceServer::GetRenderPosition(const RawAddress &device, uint3
                                                  uint32_t &timeStamp)
 {
     HILOGI("starts");
-    return pimpl->a2dpSrcService_->GetRenderPosition(device, delayValue, sendDataSize, timeStamp);
+    int ret = pimpl->a2dpSrcService_->GetRenderPosition(device, delayValue, sendDataSize, timeStamp);
     HILOGI("delayValue = %{public}u, sendDataSize = %{public}lu, timeStamp = %{public}u", delayValue, sendDataSize,
         timeStamp);
+    return ret;
 }
 
 int BluetoothA2dpSourceServer::OffloadStartPlaying(const RawAddress &device, const std::vector<int32_t> &sessionsId)
