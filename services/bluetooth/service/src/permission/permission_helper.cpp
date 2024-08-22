00af8e07b8476771fdc11dde620ff319029cc386
hui <lizhihui10@huawei.com>
2024-08-22 20:35:51 +0800

    Permission verification （cherry picked commit from <gitee.com//openharmony/communication_bluetooth_service/commit/1f67ff6658540f1364a00c59ed3f679de4b7f4f6>
    Signed-off-by: hui <lizhihui10@huawei.com>

--- services/bluetooth/service/src/permission/permission_helper.cpp
+++ services/bluetooth/service/src/permission/permission_helper.cpp
@@ -72,7 +72,7 @@ int PermissionHelper::VerifyPermission(const std::string &permissionName, const 
 int PermissionHelper::VerifyUseBluetoothPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.USE_BLUETOOTH", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -81,7 +81,7 @@ int PermissionHelper::VerifyUseBluetoothPermission(const int &pid, const int &ui
 int PermissionHelper::VerifyDiscoverBluetoothPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.DISCOVER_BLUETOOTH", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -90,7 +90,7 @@ int PermissionHelper::VerifyDiscoverBluetoothPermission(const int &pid, const in
 int PermissionHelper::VerifyManageBluetoothPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.MANAGE_BLUETOOTH", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -99,7 +99,7 @@ int PermissionHelper::VerifyManageBluetoothPermission(const int &pid, const int 
 int PermissionHelper::VerifyLocationPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.LOCATION", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -108,7 +108,7 @@ int PermissionHelper::VerifyLocationPermission(const int &pid, const int &uid)
 int PermissionHelper::VerifyApproximatelyPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.APPROXIMATELY_LOCATION", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -116,8 +116,15 @@ int PermissionHelper::VerifyApproximatelyPermission(const int &pid, const int &u
 
 int PermissionHelper::VerifyAccessBluetoothPermission(const int &pid, const int &uid)
 {
+    HapTokenInfo hapTokenInfo;
+    auto callerToken = IPCSkeleton::GetCallingTokenID();
+    if (Security::AccessToken::AccessTokenKit::GetHapTokenInfo(callerToken, hapTokenInfo) == RET_SUCCESS) {
+        if (hapTokenInfo.bundleName == "com.ohos.settings" || hapTokenInfo.bundleName == "com.ohos.systemui") {
+            return PERMISSION_GRANTED;
+        }
+    }
     if (VerifyPermission("ohos.permission.ACCESS_BLUETOOTH", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -126,7 +133,7 @@ int PermissionHelper::VerifyAccessBluetoothPermission(const int &pid, const int 
 int PermissionHelper::VerifyGetBluetoothLocalMacPermission(const int &pid, const int &uid)
 {
     if (VerifyPermission("ohos.permission.GET_BLUETOOTH_LOCAL_MAC", pid, uid) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -135,7 +142,7 @@ int PermissionHelper::VerifyGetBluetoothLocalMacPermission(const int &pid, const
 int PermissionHelper::VerifyUseBluetoothPermission(const std::uint32_t  &tokenID)
 {
     if (VerifyPermission("ohos.permission.USE_BLUETOOTH", tokenID) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
@@ -144,7 +151,7 @@ int PermissionHelper::VerifyUseBluetoothPermission(const std::uint32_t  &tokenID
 int PermissionHelper::VerifyDiscoverBluetoothPermission(const std::uint32_t  &tokenID)
 {
     if (VerifyPermission("ohos.permission.DISCOVER_BLUETOOTH", tokenID) == PERMISSION_DENIED) {
-        return PERMISSION_GRANTED;
+        return PERMISSION_DENIED;
     }
 
     return PERMISSION_GRANTED;
